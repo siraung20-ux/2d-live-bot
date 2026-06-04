780a8be8c627111b220c519f9c9d5e79af35b2e9
Antony <siraung20@gmail.com>
2026-06-04 15:15:59 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import os
 import requests
 
-url = "https://thai-lotto-new-api.p.rapidapi.com/api/v1/live"
+API_KEY = os.getenv("FINNHUB_API_KEY")
 
-headers = {
-    "x-rapidapi-host": "thai-lotto-new-api.p.rapidapi.com",
-    "x-rapidapi-key": os.getenv("RAPIDAPI_KEY"),
-    "User-Agent": "Mozilla/5.0"
-}
+url = "https://finnhub.io/api/v1/quote?symbol=%5ESET.BK&token=" + API_KEY
 
-r = requests.get(url, headers=headers, timeout=60)
+r = requests.get(url, timeout=30)
 
 print("STATUS:", r.status_code)
 print(r.text)
